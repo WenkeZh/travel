c9f77a4003f0e04844fad6a68df07eeb6b47aed6
WenkeZh <1034619148@qq.com>
2018-07-25 10:59:16 +0800

    commit

--- travel.cpp
+++ travel.cpp
@@ -9,13 +9,14 @@ struct travel_time
 };
 const int Mins_per_hr=60;
 
-travel_time sum(travel_time *,int,int );
-void show_time(travel_time);
+void  sum(const travel_time *, travel_time *,int,int );
+void show_time(const travel_time *);
 
 int main()
 {
 	int n,h,m;
 	int num1,num2;
+	travel_time total={0,0};
 	cout<<"Total-day's number: ";
 	cin>>n;
 	
@@ -41,28 +42,27 @@ int main()
 	{
 		input_data_test(num1,num2);
 	}
-	travel_time trip=sum(p1,num1-1,num2-1);
+	sum(p1,&total,num1-1,num2-1);
 	cout<<"From the "<<num1<<" to the "<<num2<< "total spend :";
-	show_time(trip);
+	show_time(& total);
 	delete [] p1;
 	system("pause");
 	return 0;
 }
 
-travel_time sum(travel_time *t,int t1,int t2)
+void sum(const travel_time *t, travel_time *total,int t1,int t2)
 {
-	travel_time total={0,0};
+	int tmins=0,thours=0;
 	for(int i=t1;i<=t2;++i)
 	{
-		total.mins+=t[i].mins;
-		total.hours+=t[i].hours;
+		tmins+=t[i].mins;
+		thours+=t[i].hours;
 	}
-	total.hours=total.hours+total.mins/Mins_per_hr;
-	total.mins=total.mins%Mins_per_hr;
-	return total;
+	(*total).hours=thours+tmins/Mins_per_hr;
+	(*total).mins=tmins%Mins_per_hr;
 }
 
-void show_time(travel_time t)
+void show_time(const travel_time *total)
 {
-	cout<<t.hours<<"hours,"<<t.mins<<"minutes"<<endl;
+	cout<<(*total).hours<<"hours,"<<(*total).mins<<"minutes"<<endl;
 }
